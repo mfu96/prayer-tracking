320d5ed8cd8a4ef0fce55eef159011b75b3adf3f
mfu96 <furkan42uslu@gmail.com>
2025-12-13 16:21:00 +0300

    131225

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,6 +4,13 @@ const config: CapacitorConfig = {
   appId: 'com.mfunet.eMesai',
   appName: 'e-Mesai',
   webDir: 'www'
-};
+,
+    android: {
+       buildOptions: {
+          keystorePath: 'c:\Front-End\prayer-tracking\android\app\e-Mesai.keystore',
+          keystoreAlias: 'anahtar-e-Mesai',
+       }
+    }
+  };
 
 export default config;
